fb54a226e20dfa4bf012a9a9acf6c8f17b238e5d
Celeztico <153655968+Celeztico@users.noreply.github.com>
2025-12-02 00:51:58 +0530

    added styling

--- chatbout-ui-react/index.js
+++ chatbout-ui-react/index.js
@@ -56,16 +56,17 @@ function ChatInput({ chatMessages, setChatMessages }) {
 
 
     return (
-        <>
+        <div className="chat-input-container">
             <input 
+                className="chat-input"
                 type="text" 
                 placeholder="Type your message..." 
                 onChange={saveInputText}
                 value={inputText}
                 onKeyDown={handleKeyDown}
                 />
-            <button onClick={sendMessage}>Send</button>
-        </>
+            <button className="send-button" onClick={sendMessage}>Send</button>
+        </div>
     );
 }
 
@@ -75,10 +76,16 @@ function ChatMessage({ text, sender }) {
     // const sender = props.sender;   all these are same as below line
     // const { text, sender } = props;  same as that in function parameter
     return (
-        <div>
-            {sender === 'bot' && <img src="robot.png" width="50" />}
-            {text}
-            {sender === 'user' && <img src="user.png" width="50" />}
+        <div className={sender === 'bot' ? "chat-message-bot" : "chat-message-user"}>
+            {sender === 'bot' && (
+                <img src="robot.png" className="chat-profile-pic" />
+            )}
+            <div className="chat-message-text">
+                {text}
+            </div>
+            {sender === 'user' && (
+                <img src="user.png" className="chat-profile-pic" />
+            )}
         </div>
     );
 }
@@ -86,7 +93,7 @@ function ChatMessage({ text, sender }) {
 // render list of chat messages
 function ChatMessages({ chatMessages }) {
     return (
-        <>
+        <div className="chat-output-container">
             {chatMessages.map((chatMessage) => {
                 return (
                     <ChatMessage 
@@ -96,7 +103,7 @@ function ChatMessages({ chatMessages }) {
                     />
                 );
             })}
-        </>
+        </div>
     );
 }
 
@@ -123,16 +130,15 @@ function App(){
     //const chatMessages = array[0]; these two are same as above line
     //const setChatMessages = array[1];
     return (
-        <>
+        <div className="app-container">
+            <ChatMessages 
+                chatMessages={chatMessages}
+            />
             <ChatInput 
                 chatMessages={chatMessages} 
                 setChatMessages={setChatMessages}
             />
-            <ChatMessages 
-                chatMessages={chatMessages}
-            />
-
-        </>
+        </div>
     );
 }
 
