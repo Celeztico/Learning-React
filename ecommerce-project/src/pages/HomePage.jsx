d03b32726fd3f8e3d94d07c826dbeaa8bf73f67f
Celeztico <153655968+Celeztico@users.noreply.github.com>
2025-12-05 22:16:59 +0530

    generate html for cartitems in checkout page

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Header from '../components/Header.jsx';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
+import formatMoney from '../utils/money.js';
 import { useEffect , useState } from 'react';
 import './HomePage.css';
 
@@ -53,7 +54,7 @@ export function HomePage({ cart }) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
