36ac5fcadaddd69b1af92056e2c790aafba0e5e5
Celeztico <153655968+Celeztico@users.noreply.github.com>
2025-12-05 13:13:43 +0530

    made routes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,15 @@
 //import { useState } from 'react'
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />}/> {/* same as path='/' */}
+      <Route path='/checkout' element={<div>CheckOut</div>}/>
+    </Routes>
   )
 }
 
